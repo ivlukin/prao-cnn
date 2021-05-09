021d2df148cd2c99efa9a193fcdb45a04ea30d2b
ivlukin <Lukin2101>
2021-05-09 14:13:17 +0300

    убрано искусственное зануление 0-й частоты в спектре

--- Celestial/Ray.cpp
+++ Celestial/Ray.cpp
@@ -13,8 +13,7 @@ void Ray::calculateModulus() {
     int size = complexAmplitudes.size();
     modulus = std::vector<float>();
 
-    modulus.push_back(0);
-    for (int j = 2; j < size - 1; j += 2) {
+    for (int j = 0; j < size - 1; j += 2) {
         float real = complexAmplitudes[j] / (float) nbands;
         float imaginary = complexAmplitudes[j + 1] / (float) nbands;
         modulus.push_back(std::sqrt(real * real + imaginary * imaginary));
