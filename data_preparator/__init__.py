283f9019ab9b7d959522be83aea09ca818bf72ac
ivlukin <Lukin2101>
2021-05-11 03:18:54 +0300

    добавлены актуальное время и дата пульсаров на 2015-01-01 в часовом поясе MSK а также соответстующие конфиги

--- data_preparator/__init__.py
+++ data_preparator/__init__.py
@@ -2,6 +2,7 @@
 # дата начала наблюдений
 # длина наблюдений
 # выходная папка
+import datetime
 
 from Pulsar import Pulsar
 import json
@@ -13,15 +14,17 @@ with open("pulsars.txt") as f:
 
 def prepare_config(pulsar):
     config_data = {}
-    config_data['startDate'] = pulsar.msk_begin.strftime('%Y-%m-%d %H:%M:%S')
-    config_data['endDate'] = pulsar.msk_begin.strftime('%Y-%m-%d %H:%M:%S')
+    duration = 204.8
+    start_date_shifted = pulsar.msk_begin - datetime.timedelta(seconds=duration / 2)
+    config_data['startDate'] = start_date_shifted.strftime('%Y-%m-%d %H:%M:%S')
+    config_data['endDate'] = start_date_shifted.strftime('%Y-%m-%d %H:%M:%S')
     config_data['observationLength'] = 90
     config_data['range'] = pulsar.range
     config_data['outputPath'] = '/home/sorrow/prao-output-main/{name}/'.format(name=pulsar.name)
     config_data['mode'] = 'pnt'
     config_data["fileListPath"] = "/home/sorrow/CLionProjects/prao-cnn/01-15-list-files.txt"
     config_data["calibrationListPath"] = "/home/sorrow/CLionProjects/prao-cnn/01-15-calb.txt"
-    config_data["durationStarSeconds"] = 204.8
+    config_data["durationStarSeconds"] = duration
     config_data["summationEnabled"] = False
     config_data["writeRawData"] = False
     return config_data
